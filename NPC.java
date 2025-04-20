05e464795d5a8db21d7a8a0e1797b6148cedd92b
APCodingGoat1 <nathan11blue@gmail.com>
2025-04-19 23:30:26 -0700

    Added Game and NPC class(testing) 4/19

--- NPC.java
+++ NPC.java
@@ -1,5 +1,28 @@
 public class NPC {
-    public static void main(String[] args) {
-        System.out.println("Hello, Wld!!");
+    private String name;
+    private int loveStat = 0;
+    //private String relationship <-- relative to loveStat idk how but make it work
+    public NPC(String name, int loveStat) {
+        this.name = name;
+        this.loveStat = loveStat;
     }
+
+    public String getName(){
+        return name;
+    }
+    public int getloveStat(){
+        return loveStat;
+    }
+
+    private void setName(String name){
+        this.name = name;
+    }
+    public void setloveStat(int loveStat){
+        this.loveStat = loveStat;
+    }
+
+    public String toString() {
+        return "Name: " + this.name + "\nLove: " + this.loveStat;
+    }
+
 }
