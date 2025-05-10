8543fb4d1ea7db3b516c571f7fc864f41e34f48f
Nathan Diep <158011597+APCodingGoat1@users.noreply.github.com>
2025-05-09 20:45:39 -0700

    added comments to customButton.java

--- customButton.java
+++ customButton.java
@@ -1,7 +1,13 @@
 import java.awt.event.ActionListener;
 import javax.swing.*;
-
+/**
+ * The customButton class extends JButton to create a button with custom dimensions, 
+ * position, and an action listener. It simplifies the creation of buttons in the game.
+ */
 public class customButton extends JButton {
+    /**
+     * Constructs a customButton with the specified text, position, size, and action listener.
+    */
     public customButton(String text, int x, int y, int width, int height, ActionListener action) {
         super(text); // Set the button's text
         this.setBounds(x, y, width, height); // Set the button's position and size
